9666db542b27c57ce57031dbddc36a5cf4d51687
cretivebeom <ix2ix2@naver.com>
2024-06-30 17:14:39 +0900

    Update _02_DateTypes.java

--- _02_DateTypes.java
+++ _02_DateTypes.java
@@ -4,12 +4,12 @@ public class _02_DateTypes {
     public static void main(String[] args) {
         System.out.println("Hello World");    // 문자열
         System.out.println("안녕하세요");
-        System.out.println(12);    //숫자 자료형
+        System.out.println(12);    // 숫자 자료형
         System.out.println(-34);
         System.out.println(3.14);
-        System.out.println(true);    //불리한 자료형
+        System.out.println(true);    // 불리한 자료형
         System.out.println(false);
 
-        System.out.println(123 + 345);
+        System.out.println(123 + 345);    // 간단한 연산
     }
 }
